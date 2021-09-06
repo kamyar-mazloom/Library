77edd16c5015a20f60004c972e6deddc80340064
Kamyar mazloom <mazloom1994@gmail.com>
2021-09-06 09:53:24 +0430

    delete button (ver.1)
    Not working correctly

--- Script.js
+++ Script.js
@@ -22,7 +22,7 @@ function addBookToLibrary() {
   cardCreation()
   }
 };
-// //event listener for form submit button 
+// event listener for form submit button 
 document.addEventListener('DOMContentLoaded', ()=>{
   document.getElementById('submitBtn-id').addEventListener('click', addBookToLibrary);
 });
@@ -33,13 +33,16 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 } 
+//close function
+event
 // Card creation 
 function cardCreation (){
 for (let i=0; i<myLibrary.length; i++){
   let domBody = document.getElementById("body-id")
-
+  
   let domCard = document.createElement("div")
   domCard.setAttribute("class","card")
+  domCard.setAttribute("id", i)
   domBody.append(domCard)
 
   let domTitle = document.createElement("H2")
@@ -56,6 +59,29 @@ for (let i=0; i<myLibrary.length; i++){
   domPages.setAttribute("class","book-page")
   domPages.innerHTML = "<span>Number of pages: </span>" + myLibrary[i].pages;
   domCard.append(domPages)
+
+  let domStatus = document.createElement("span")
+  domStatus.setAttribute("class","status")
+  domStatus.innerHTML = "<span>toggle as read<span>"
+  domCard.appendChild(domStatus)
+
+  let domSwitch = document.createElement("label")
+  domSwitch.setAttribute("class","switch")
+  domCard.appendChild(domSwitch)
+
+  let domCheckbox = document.createElement("input")
+  domCheckbox.setAttribute("type","checkbox")
+  domSwitch.appendChild(domCheckbox)
+
+  let domSlider = document.createElement("span")
+  domSlider.setAttribute("class","slider round")
+  domSwitch.appendChild(domSlider)
+
+  let domClose = document.createElement("button")
+  domClose.setAttribute("class","closeBtn")
+  domClose.setAttribute("id", i)
+  domClose.innerHTML = "x";
+  domCard.append(domClose)
 }}
 window.onload = cardCreation()
 //Remove child nodes 
@@ -63,4 +89,12 @@ function removeAllChildNodes(parent) {
   while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
   }
+}
+
+let buttons = document.getElementsByTagName("button");
+let buttonsCount = buttons.length;
+for (let i = 0; i < buttonsCount; i++) {
+    buttons[i].onclick = function(e) {
+        alert(this.id);
+    };
 }
